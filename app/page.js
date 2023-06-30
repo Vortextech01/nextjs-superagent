5389758b8f85d21477f765605d01e85137340364
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-06-29 23:13:11 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   const onSubmit = useCallback(async (values) => {
     const { message } = values;
     const newMessage = { type: 'user', content: message };
-    setConversation([newMessage, ...conversation]);
+    setConversation([...conversation, newMessage]);
 
     setMessage(message);
     setAiResponse();
@@ -29,7 +29,7 @@ export default function Home() {
     });
 
     const newResponse = { type: 'ai', content: data };
-    setConversation([newResponse, ...conversation]);
+    setConversation([...conversation, newResponse]);
 
     setAiResponse(data);
   }, [conversation]);
@@ -52,10 +52,17 @@ export default function Home() {
         </div>
         <div className="flex flex-col space-y-4">
           {conversation.map((message, index) => (
-            <div key={index} className={`flex space-x-2 text-${message.type === 'user' ? 'orange' : 'green'}-500 font-bold text-md`}>
+            <div
+              key={index}
+              className={`flex space-x-2 text-${message.type === 'user' ? 'orange' : 'green'}-500 font-bold text-md`}
+            >
               <div className="flex items-center space-x-2">
                 <p className="text-white">
-                  <svg className="fill-current opacity-75 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                  <svg
+                    className="fill-current opacity-75 h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
                     <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
                   </svg>
                 </p>
@@ -90,4 +97,3 @@ export default function Home() {
     </main>
   );
 }
-``
